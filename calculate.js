deafd37c739c07ecc412f8f2efd4569176827dbf
Holmes7 <ashutoshpatkar@yahoo.in>
2020-09-18 23:02:17 +0530

    Basic Layout

---
+++ calculate.js
@@ -0,0 +1,10 @@
+let dataArray = []
+
+function calculateRatingChange(){
+	let contestCode = document.querySelector("#code").value
+	let username = document.querySelector("#username").value
+	console.log(contestCode)
+	console.log(username)
+}
+
+function  
